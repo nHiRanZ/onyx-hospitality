1619bb72766efa83fde2a04552f0bd10ba4394bc
yohanranuk <yohanranuk@gmail.com>
2017-01-11 03:11:49 +0530

    CSS improvements to the menu

--- header.php
+++ header.php
@@ -3,12 +3,12 @@
 
 
     <ul>
-        <center><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:175px;height:65px; margin: 10px 10px 10px 5px"></center>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="pubs.php">Pubs</a></li>
-        <li><a href="restaurants.php">Restaurants</a></li>
-        <li><a href="hotels.php">Hotels</a></li>
+        <center><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:175px;height:65px; margin: 40px 10px 20px 5px"><hr></center>
+        <li><a class="active" href="index.php">Home</a><hr></li>
+        <li><a href="about.php">About</a><hr></li>
+        <li><a href="pubs.php">Pubs</a><hr></li>
+        <li><a href="restaurants.php">Restaurants</a><hr></li>
+        <li><a href="hotels.php">Hotels</a><hr></li>
     </ul>
 
 
