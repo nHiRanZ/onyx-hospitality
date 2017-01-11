83daac5dff8f76a083dc98384d191005cec28cbe
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-11 21:48:26 +0530

    link added to logo

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header class="header-main">
     <ul>
-        <center><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:170px;height:60px; margin: 40px 10px 20px 5px"><hr></center>
+        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:170px;height:60px; margin: 40px 10px 20px 5px"></a><hr></center>
         <li><a href="index.php">PROFILE<i class="fa fa-user-o" style="float: right"></i></a><hr></li>
         <li><a href="about.php">RESERVATIONS<i class="fa fa-user-o" style="float: right"></i></a><hr></li>
         <li><a href="pubs.php">ABOUT US<i class="fa fa-beer" style="float: right"></i></a><hr></li>
