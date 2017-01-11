4aedb072b1d99855e49715dbc16373404db3dbfa
Crishane Perera <Crishane Perera>
2017-01-12 03:57:51 +0530

    Added the sql file

---
+++ sql/onyx.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 11, 2017 at 10:23 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onyx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pubs`
+--
+
+CREATE TABLE `pubs` (
+  `pubID` int(5) NOT NULL,
+  `pubName` varchar(20) NOT NULL,
+  `details` varchar(50) NOT NULL,
+  `reservations` int(3) NOT NULL,
+  `reviews` decimal(3,0) NOT NULL,
+  `location` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `QID` int(3) NOT NULL,
+  `question` int(50) NOT NULL,
+  `ans1` varchar(15) NOT NULL,
+  `ans2` varchar(15) NOT NULL,
+  `ans3` varchar(15) NOT NULL,
+  `ans4` varchar(15) NOT NULL,
+  `final` int(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(15) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `location` varchar(20) NOT NULL,
+  `points` int(5) NOT NULL,
+  `check-ins` int(5) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `gameID` int(4) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstat`
+--
+
+CREATE TABLE `userstat` (
+  `userID` int(10) NOT NULL,
+  `gameID` int(5) NOT NULL,
+  `question1` varchar(50) NOT NULL,
+  `question2` varchar(50) NOT NULL,
+  `question3` varchar(50) NOT NULL,
+  `question4` varchar(50) NOT NULL,
+  `question5` varchar(50) NOT NULL,
+  `question6` varchar(50) NOT NULL,
+  `question7` varchar(50) NOT NULL,
+  `question8` varchar(50) NOT NULL,
+  `question9` varchar(50) NOT NULL,
+  `question10` varchar(50) NOT NULL,
+  `answerCorrect` int(2) NOT NULL,
+  `answerWrong` int(2) NOT NULL,
+  `Total` int(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pubs`
+--
+ALTER TABLE `pubs`
+  ADD PRIMARY KEY (`pubID`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`QID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
