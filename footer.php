0dae986dd01b88b0cfada7dca79043309d5ff2c4
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-12 01:13:21 +0530

    footer and login page added/complete

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
 <!-- Bootstrap -->
 <script src="js/bootstrap.js"></script>
 <script src="js/npm.js"></script>
+<div class="footer">
+    <center>
+        <p>ONYX Hospitality Inc. 2017</p>
+        <span><a href="about.php">About</a>
+            <a href="contact.php">Contact us</a>
+            <a>Facebook</a>
+            <a>Twitter</a> </span>
+    </center>
+</div>
--- login.php
+++ login.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Lishni
- * Date: 1/11/2017
- * Time: 10:19 PM
- */
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>Login | ONYX Hospitality</title>
+
+    <?php include('head.php'); ?>
+</head>
+
+<body>
+
+<div class="page-image-area">
+    <img src="img/login/login-background.jpg"style="width: 100%;">
+</div>
+<div class="page-content-area">
+
+</div>
+
+
+
+<!-- FOOTER SECTION -->
+<?php include('footer.php'); ?>
+<!-- //END -->
+
+</body>
+
+</html>
