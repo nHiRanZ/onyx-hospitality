9c51e51883cff93d2d56fde2a5105679cb9be31d
yohanranuk <yohanranuk@gmail.com>
2017-01-12 02:01:08 +0530

    made footer text bold

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 <script src="js/npm.js"></script>
 <div class="footer">
     <center>
-        <p>ONYX Hospitality Inc. 2017</p>
+        <p style="font-weight: bold">ONYX Hospitality Inc. 2017</p>
         <span>
-            <a href="about.php" style="color:white;">About 	&nbsp;</a>
-            <a href="contact.php" style="color:white;"> Contact us 	&nbsp;</a>
-            <a style="color:white;"> Facebook 	&nbsp;</a>
-            <a style="color:white;"> Twitter 	&nbsp;</a>
+            <a href="about.php" style="color:white;">About &nbsp;</a>
+            <a href="contact.php" style="color:white;"> Contact us &nbsp;</a>
+            <a style="color:white;"> Facebook &nbsp;</a>
+            <a style="color:white;"> Twitter &nbsp;</a>
         </span>
     </center>
 </div>
