4bcf9e8a43228932994db468dca7b45b97e13c06
lishniii <lishni.tilakaratne@gmail.com>
2017-01-11 16:35:24 +0530

    basic layout of home page created

--- header.php
+++ header.php
@@ -4,8 +4,9 @@
 
     <ul>
         <center><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:175px;height:65px; margin: 40px 10px 20px 5px"><hr></center>
-        <li><a class="active" href="index.php">Home</a><hr></li>
-        <li><a href="about.php">About<i class="fa fa-user-o" style="float-right"></i></a><hr></li>
+        <span class="icon-bar"><li><a class="active" href="index.php">Home</a><hr></li></span>
+        <span class="icon-bar"><li><a href="about.php">About<i class="fa fa-user-o"></i></a><hr></li></span>
+
         <li><a href="pubs.php">Pubs<i class="fa fa-beer" style="right: 90%;"></i></a><hr></li>
         <li><a href="restaurants.php">Restaurants<i class="fa fa-cutlery" style="margin-left:50%"></i></a><hr></li>
         <li><a href="hotels.php">Hotels<i class="fa fa-bed" style="margin-left:50%"></i></a><hr></li>
--- index.php
+++ index.php
@@ -22,14 +22,15 @@
 
 <body>
 
+
 <?php
 $_SESSION["ActivePage"]  = "index";
 include('header.php');
 ?>
 
+<div class="frame-home">
 
-<div style="margin-left:25%;padding:1px 16px;">
-    <h2>Fixed Full-height Side Nav</h2>
+    <h2>Fixed lalalaaFull-height Side Nav</h2>
     <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
     <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
     <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
@@ -43,9 +44,9 @@ include('header.php');
     <i class="fa fa-car"></i>
     <i class="fa fa-car" style="font-size:48px;"></i>
     <i class="fa fa-car" style="font-size:60px;color:red;"></i>
+
+
 </div>
-
-
 
 
 <!-- FOOTER SECTION -->
