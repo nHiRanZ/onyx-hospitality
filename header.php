a1a6456373098d61ff44a83d370f88d795f9ef4d
lishniii <lishni.tilakaratne@gmail.com>
2017-01-12 10:51:01 +0530

    header link changes

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <ul>
         <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width:170px;height:60px; margin: 40px 10px 20px 5px"></a><hr></center>
         <li><a href="profile.php">PROFILE<i class="fa fa-user-o" style="float: right"></i></a><hr></li>
-        <li><a href="profile.php">PUBS<i class="fa fa-beer" style="float: right"></i></a><hr></li>
+        <li><a href="search.php">PUBS<i class="fa fa-beer" style="float: right"></i></a><hr></li>
         <li><a href="profile.php">RESTAURANTS<i class="fa fa-cutlery" style="float: right"></i></a><hr></li>
         <li><a href="profile.php">HOTELS<i class="fa fa-hotel" style="float: right"></i></a><hr></li>
         <li><a href="reservation.php">RESERVATIONS<i class="fa fa-tags" style="float: right"></i></a><hr></li>
