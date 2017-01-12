024982db78fc77c29d00039830ee83c9e36bdd50
yohanranuk <yohanranuk@gmail.com>
2017-01-12 15:04:33 +0530

    added game-tutorial page

---
+++ game-tutorial.php
@@ -0,0 +1,75 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>Game Introduction | ONYX Hospitality</title>
+
+    <?php include('head.php'); ?>
+</head>
+
+<body>
+
+<div class="page-image-area col-md-6" style="float: left">
+    <img src="img/game/game-intro.jpg"style="">
+    <div class="highlight">
+        <span><a href="index.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
+    </div>
+</div>
+<center></center>
+<div class="page-content-area col-md-6" style="float:right">
+    <div class="highlight">
+        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
+    </div>
+
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+                <center><p>Welcome to the Weekly Treasure Hunt Challenge by ONYX Hospitality. Up on completion you’re eligible for the prize that was mentioned in the previous page*.</p></center>
+            </div>
+        </div>
+    </div>
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+                <center><p>You have to Answer 10 Simple Questions which are related to ONYX Group of Pubs, Hotels and Restaurants. In each level, you’re given hints to accomplish the task at hand. You have 72 Hours once you start the game*. You can resume the game from your Profile.</p></center>
+            </div>
+        </div>
+    </div>
+
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+               <center> <p style="align-content: center">Good Luck!</p> </center>
+            </div>
+        </div>
+    </div>
+
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="login">
+                    <a href="index.php" ><div class="button-btn">Start</div></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+</center>
+
+
+<!-- FOOTER SECTION -->
+<?php include('footer.php'); ?>
+<!-- //END -->
+
+</body>
+
+</html>
