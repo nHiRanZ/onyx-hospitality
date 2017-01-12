b346aea1b161a180dad0167897e1109172ff0baa
lishniii <lishni.tilakaratne@gmail.com>
2017-01-12 16:02:32 +0530

    game tutorial page complete

--- game-tutorial.php
+++ game-tutorial.php
@@ -16,29 +16,36 @@
 
 <body>
 
-<div class="page-image-area col-md-6" style="float: left">
-    <img src="img/game/game-intro.jpg"style="">
+<div class="page-image-area" style="float: left">
+    <img src="img/game/game-intro.jpg" style="">
     <div class="highlight">
         <span><a href="index.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
     </div>
 </div>
 <center></center>
-<div class="page-content-area col-md-6" style="float:right">
+<div class="page-content-area" style="float:right">
     <div class="highlight">
-        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
+        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo"
+                                         style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
     </div>
-
-    <div class="sub-block">
+    <div class="main-block">
+        <div class="row">
+        </div>
+    </div>
+    <div class="sub-block" ">
         <div class="row">
             <div class="col-sm-12">
-                <center><p>Welcome to the Weekly Treasure Hunt Challenge by ONYX Hospitality. Up on completion you’re eligible for the prize that was mentioned in the previous page*.</p></center>
+                <center><p style="margin-left: 5%; margin-right: 5%;">Welcome to the Weekly Treasure Hunt Challenge by ONYX Hospitality. Up on completion you’re
+                        eligible for the prize that was mentioned in the previous page*.</p></center>
             </div>
         </div>
     </div>
     <div class="sub-block">
         <div class="row">
             <div class="col-sm-12">
-                <center><p>You have to Answer 10 Simple Questions which are related to ONYX Group of Pubs, Hotels and Restaurants. In each level, you’re given hints to accomplish the task at hand. You have 72 Hours once you start the game*. You can resume the game from your Profile.</p></center>
+                <center><p style="margin-left: 5%; margin-right: 5%;">You have to Answer 10 Simple Questions which are related to ONYX Group of Pubs, Hotels and
+                        Restaurants. In each level, you’re given hints to accomplish the task at hand. You have 72 Hours
+                        once you start the game*. You can resume the game from your Profile.</p></center>
             </div>
         </div>
     </div>
@@ -46,7 +53,7 @@
     <div class="sub-block">
         <div class="row">
             <div class="col-sm-12">
-               <center> <p style="align-content: center">Good Luck!</p> </center>
+                <center><p style="align-content: center">Good Luck!</p></center>
             </div>
         </div>
     </div>
@@ -55,7 +62,9 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="login">
-                    <a href="index.php" ><div class="button-btn">Start</div></a>
+                    <a href="index.php">
+                        <div class="button-btn">Start</div>
+                    </a>
                 </div>
             </div>
         </div>
