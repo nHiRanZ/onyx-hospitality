5464edd5085e4c05ac0cb7fef9310b6140b93b93
Crishane Perera <crishane.r.perera@gmail.com>
2017-01-13 02:30:55 +0530

    Updated the search File

--- search.php
+++ search.php
@@ -33,12 +33,13 @@
             </div>
         </div>
     </div>
+
     <div class="block">
         <div class="row">
             <div class="col-sm-12">
                 <div class="col-sm-12"><h3>LOCATE PUBS</h3></div>
                 <span>
-                    //giving the options to the user
+
                 <div class="col-sm-4">
                     PROVINCE<br>
                 <select>
