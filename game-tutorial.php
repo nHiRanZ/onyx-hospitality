d0397fcb86da874978d8b876b57b86265c6a9983
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-13 02:00:53 +0530

    game completion page added

--- game-tutorial.php
+++ game-tutorial.php
@@ -12,66 +12,50 @@
     <title>Game Introduction | ONYX Hospitality</title>
 
     <?php include('head.php'); ?>
+
 </head>
 
-<body>
+<body style="height: 100%">
 
-<div class="page-image-area" style="float: left">
-    <img src="img/game/game-intro.jpg" style="">
+<div class="container col-md-12 col-sm-12 col-xs-12" style="">
     <div class="highlight">
-        <span><a href="game-promotion.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
-    </div>
-</div>
-<center></center>
-<div class="page-content-area" style="float:right">
-    <div class="highlight">
-        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo"
-                                         style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
-    </div>
-    <div class="main-block">
-        <div class="row">
-        </div>
-    </div>
-    <div class="sub-block" ">
-        <div class="row">
-            <div class="col-sm-12">
-                <center><p style="margin-left: 5%; margin-right: 5%;">Welcome to the Weekly Treasure Hunt Challenge by ONYX Hospitality. Up on completion you’re
-                        eligible for the prize that was mentioned in the previous page*.</p></center>
-            </div>
-        </div>
-    </div>
-    <div class="sub-block">
-        <div class="row">
-            <div class="col-sm-12">
-                <center><p style="margin-left: 5%; margin-right: 5%;">You have to Answer 10 Simple Questions which are related to ONYX Group of Pubs, Hotels and Restaurants. In each level, you’re given hints to accomplish the task at hand. You have 72 Hours once you start the game*. You can resume the game from your Profile.</p></center>
-            </div>
-        </div>
+        <span><a href="index.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
+        <a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" class="highlightlogo"></a>
     </div>
 
-    <div class="sub-block">
-        <div class="row">
-            <div class="col-sm-12">
-                <center><p style="align-content: center">Good Luck!</p></center>
-            </div>
-        </div>
+    <div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: blue; padding:0px 0px;">
+
+        <img src="img/game/game-intro.jpg" style="">
     </div>
 
-    <div class="sub-block">
+    <div class="col-md-6 col-sm-6 col-xs-12 containertext center" style="">
         <div class="row">
-            <div class="col-sm-12">
-                <div class="button-section">
-                    <a href="game.php">
-                        <div class="button-black">START</div>
-                    </a>
-                </div>
+            <div class="col-sm-12 col-md-12 col-xs-12">
+                <center><p style="margin-left: 5%; margin-right: 5%; margin-bottom: 5%;">Welcome to the Weekly Treasure
+                        Hunt Challenge by ONYX Hospitality. Up on completion you’re eligible for the prize that was
+                        mentioned in the previous page*.</p>
+
+                    <p style="margin-left: 5%; margin-right: 5%; margin-bottom: 5%;">You have to Answer 10 Simple
+                        Questions which are related to ONYX Group of Pubs, Hotels and Restaurants. In each level, you’re
+                        given hints to accomplish the task at hand. You have 72 Hours once you start the game*. You can
+                        resume the game from your Profile.</p>
+
+                    <p style="margin-left: 5%; margin-right: 5%;">Good Luck!</p>
+
+                    <div class="col-sm-12 col-md-12 col-xs-12">
+                        <div class="">
+                            <a href="game.php">
+                                <div class="button-black">START</div>
+                            </a>
+                        </div>
+                    </div>
+                </center>
             </div>
         </div>
+
     </div>
 
 </div>
-</div>
-</center>
-
 
 <!-- FOOTER SECTION -->
 <?php include('footer.php'); ?>
