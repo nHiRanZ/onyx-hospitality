0eebea8b583b90dec7993e24e393db34b02f06fc
lishniii <lishni.tilakaratne@gmail.com>
2017-01-12 10:49:37 +0530

    search page added and other changes made

---
+++ pub.php
@@ -0,0 +1,67 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>Pub | ONYX Hospitality</title>
+
+    <?php include('head.php'); ?>
+</head>
+
+
+<body>
+
+<div class="page-image-area">
+    <img src="img/pubs/pubs-background.jpg"style="width: 110%;">
+    <div class="highlight">
+        <span><a href="search.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
+    </div>
+</div>
+<div class="page-content-area">
+    <div class="highlight">
+        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
+    </div>
+    <div class="main-block">
+        <div class="row">
+            <div class="col-sm-12">
+            </div>
+        </div>
+    </div>
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+
+            </div>
+        </div>
+    </div>
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+
+            </div>
+        </div>
+    </div>
+    <div class="sub-block">
+        <div class="row">
+            <div class="col-sm-12">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- FOOTER SECTION -->
+<?php include('footer.php'); ?>
+<!-- //END -->
+
+</body>
+
+</html>
