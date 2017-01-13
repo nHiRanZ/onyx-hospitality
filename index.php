2530ec8566bb3b456fded4c5ae752fdcc31a010a
yohanranuk <yohanranuk@gmail.com>
2017-01-13 05:38:41 +0530

    Index half completed

--- index.php
+++ index.php
@@ -13,68 +13,109 @@
 
     <?php include('head.php'); ?>
 
-    <style>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </style>
-
 </head>
-
 
 <body>
 
 
-<?php
-$_SESSION["ActivePage"]  = "index";
-include('header.php');
-?>
+<div class="container col-md-12 col-sm-12 col-xs-12" style="">
 
-<div class="frame-home">
-    <div class="main-block">
-    <div class="row">
-        <div class="col-sm-12">
-            <img src="img/home/outing-background.jpg"style="width: 100%; background:rgba(0,0,0,0.8); opacity: 6;">
-            <div class="highlight" style="height: 25%;">
-                <span>
-                <a href="location.php"><i class="fa fa-map-pin fa-2x" style="width: 30px; margin-top: 4%; margin-left: 5%; float: left""></i></a>
-                <a href="profile.php"><img src="img/home/user.png" alt="Logo" style="width: 30px; margin-top: 4%; margin-right: 10%; float: right"></a>
-                    </span>
-                <div class="special-promo">
-                    <center>
-                        <h4>SPECIAL PROMOTION</h4>
-                        <h6>Play our special promotion Treasure Hunt <br> and stand a chance to win special prizes</h6>
-                        <div class="game-promotion">
-                            <a href="game-promotion-WIP.php" ><div class="button-game-promo">PLAY</div></a>
+
+    <div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
+
+        <?php
+        $_SESSION["ActivePage"] = "index";
+        include('header.php');
+        ?>
+    </div>
+
+    <div class="col-md-6 col-sm-6 col-xs-12 containertext center" style="padding:0px 0px">
+        <div class="highlightIndex" style="margin:0px 0px; padding:0px 0px;">
+            <span>
+                <a href="location.php"><i class="fa fa-map-pin fa-2x"
+                                          style="width: 30px; margin-top: 4%; margin-left: 5%; float: left""></i></a>
+                <a href="profile.php"><img src="img/home/user.png" alt="Logo"
+                                           style="width: 30px; margin-top: 4%; margin-right: 10%; float: right"></a>
+        </div>
+
+        <!--        <div class="row col-md-12" style="height: 50%">-->
+        <!--            <img src="img/home/outing-background.jpg" style="width: 100%; background:rgba(0,0,0,0.8); opacity: 6; padding-top: 50%">-->
+        <!--        </div>-->
+
+        <div class="main-block col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <img src="img/home/outing-background.jpg"
+                         style="width: 100%; background:rgba(0,0,0,0.8); opacity: 6;">
+
+
+                        <div class="special-promo">
+                            <center>
+                                <h4>SPECIAL PROMOTION</h4>
+                                <h6>Play our special promotion Treasure Hunt <br> and stand a chance to win special
+                                    prizes</h6>
+                                <div class="game-promotion">
+                                    <a href="game-promotion-WIP.php">
+                                        <div class="button-game-promo">PLAY</div>
+                                    </a>
+                                </div>
+                            </center>
                         </div>
-                    </center>
+
                 </div>
             </div>
         </div>
-    </div>
-    </div>
-    <div class="block">
-    <div class="row">
-        <div class="col-sm-12">
-            <img src="img/home/pubs.jpg"style="width: 100%;">
+        <div class="block">
+            <div class="row">
+                <div class="col-sm-12">
+                    <img src="img/home/pubs.jpg" style="width: 100%;">
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-    <div class="block">
-    <div class="row">
-        <div class="col-sm-12">
-            <img src="img/home/restaurants.jpg"style="width: 100%;">
+        <div class="block">
+            <div class="row">
+                <div class="col-sm-12">
+                    <img src="img/home/restaurants.jpg" style="width: 100%;">
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-    <div class="block">
-    <div class="row">
-        <div class="col-sm-12">
-            <img src="img/home/hotels.jpg"style="width: 100%;">
+        <div class="block">
+            <div class="row">
+                <div class="col-sm-12">
+                    <img src="img/home/hotels.jpg" style="width: 100%;">
+                </div>
+            </div>
         </div>
+
+
+        <!--        <div class="row" style="height:50%">-->
+        <!--            <div class="col-sm-12 col-md-12 col-xs-12">-->
+        <!--                <center>-->
+        <!--                    <div class="row">-->
+        <!--                        <div class="col-sm-12">-->
+        <!--                            <img src="img/home/pubs.jpg" style="height:300px">-->
+        <!--                        </div>-->
+        <!--                    </div>-->
+        <!---->
+        <!--                    <div class="row">-->
+        <!--                        <div class="col-sm-12">-->
+        <!--                            <img src="img/home/restaurants.jpg" style="height:300px">-->
+        <!--                        </div>-->
+        <!--                    </div>-->
+        <!---->
+        <!---->
+        <!--                    <div class="row">-->
+        <!--                        <div class="col-sm-12">-->
+        <!--                            <img src="img/home/hotels.jpg" style="height:300px">-->
+        <!--                        </div>-->
+        <!--                    </div>-->
+        <!--                </center>-->
+        <!--            </div>-->
+        <!--        </div>-->
+
     </div>
-    </div>
+
 </div>
-
-
 
 <!-- FOOTER SECTION -->
 <?php include('footer.php'); ?>
