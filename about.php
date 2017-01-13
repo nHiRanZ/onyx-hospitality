a7231370e3f22c31993d957c8ac8a90555bedd2f
yohanranuk <yohanranuk@gmail.com>
2017-01-13 07:46:15 +0530

    completed about page

--- about.php
+++ about.php
@@ -12,51 +12,51 @@
     <title>About | ONYX Hospitality</title>
 
     <?php include('head.php'); ?>
+
 </head>
 
+<body style="height: 100%">
 
-<body>
-
-<div class="page-image-area">
-    <img src="img/about/about.jpg"style="width: 110%;">
+<div class="container col-md-12 col-sm-12 col-xs-12" style="">
     <div class="highlight">
         <span><a href="index.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
+        <a href="profile.php"><img src="img/home/user.png" alt="Logo"
+                                   style="width: 30px; margin-top: 4%; margin-right: 10%; float: right"></a>
+
+<!--        <div class="highlightIndex" style="margin:0px 0px; padding:0px 0px;">-->
+<!--            <span>-->
+<!--                <a href="location.php"><i class="fa fa-map-pin fa-2x"-->
+<!--                                          style="width: 30px; margin-top: 4%; margin-left: 5%; float: left""></i></a>-->
+<!--                <a href="profile.php"><img src="img/home/user.png" alt="Logo"-->
+<!--                                           style="width: 30px; margin-top: 4%; margin-right: 10%; float: right"></a>-->
+<!--        </div>-->
     </div>
+
+    <div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: blue; padding:0px 0px;">
+
+        <img src="img/about/about.jpg" style="">
+    </div>
+
+    <div class="col-md-6 col-sm-6 col-xs-12 containertext center" style="overflow: scroll; padding-top:25%">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xs-12">
+                <center><p style="margin-left: 5%; margin-right: 5%; margin-bottom: 5%;">Lorem ipsum dolor sit amet, mei fugit iudico everti ea, cu per errem suscipiantur, periculis intellegam omittantur has ei. Eum in wisi consul epicuri, eum in eros choro putent. Eu semper forensibus qui, ei quo magna solum. Vocent regione constituto cu vim. At facete partiendo comprehensam pro, nostrum concludaturque et usu.</p>
+
+                    <p style="margin-left: 5%; margin-right: 5%; margin-bottom: 5%;">Ea eum sanctus recusabo omittantur, facilisi tractatos nec in. Ex malorum pertinax intellegam qui. Est in semper praesent, vis vivendo gloriatur ne. Ne mea porro sententiae, brute partem accusata eu duo.</p>
+
+                    <p style="margin-left: 5%; margin-right: 5%;">Animal appetere sed cu, his munere mentitum inimicus cu. Duis meliore rationibus sed te. Et cum adipisci sententiae assueverit, has ne harum complectitur. Wisi libris antiopam no eum, primis mollis sea ei. Vero assum possit ius at, eu vitae quidam ancillae vim. Oratio impetus assentior mel eu, magna justo quo ex.</p>
+
+                    <p style="margin-left: 5%; margin-right: 5%;">Elit nonumy has ne, et eam facilis electram assueverit. An essent dissentiunt vix, mel cu senserit splendide. Nibh doctus nusquam ei per, nec melius alienum consequat eu, pro ne dicunt possim. Sit cu accusata incorrupte. Ad graece ponderum vel, ad dolorem blandit disputando pro, atqui reformidans ei sed. Has hinc facete appetere ea.
+                    </p>
+                    <p style="margin-left: 5%; margin-right: 5%;">Quo id corpora dissentias, quando dicunt senserit nec ne. Ex quo mundi albucius, vix libris suavitate efficiantur no. Ullum velit tantas usu cu, clita hendrerit est te. Te qui veniam oblique docendi, postea recusabo ea sed, ad omnium eripuit vix.</p>
+
+                </center>
+            </div>
+        </div>
+
+    </div>
+
 </div>
-<div class="page-content-area">
-    <div class="highlight">
-        <center><a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" style="width: 80px; margin: 24px 10px 20px 5px;"></a></center>
-    </div>
-    <div class="main-block">
-        <div class="row">
-            <div class="col-sm-12">
-            </div>
-        </div>
-    </div>
-    <div class="sub-block">
-        <div class="row">
-            <div class="col-sm-12">
-
-            </div>
-        </div>
-    </div>
-    <div class="sub-block">
-        <div class="row">
-            <div class="col-sm-12">
-
-            </div>
-        </div>
-    </div>
-    <div class="sub-block">
-        <div class="row">
-            <div class="col-sm-12">
-
-            </div>
-        </div>
-    </div>
-</div>
-
-
 
 <!-- FOOTER SECTION -->
 <?php include('footer.php'); ?>
