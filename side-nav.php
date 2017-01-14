ae6de11541654a8badd6b34feeea73cb9ef17aab
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-15 01:26:13 +0530

    side nav updated

---
+++ side-nav.php
@@ -0,0 +1,11 @@
+<header class="header-main-test">
+    <div class="col-md-12 col-sm-12 col-xs-12" style="padding:0px 0px 0px 0px;">
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</header>
