7fca8bf6b3dbe6f5316c410979c13ac1018c7d25
Crishane Perera <crishane.r.perera@gmail.com>
2017-01-15 04:00:52 +0530

    updates related to fav

--- misty-pub.php
+++ misty-pub.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" type="text/css" href="css/fav.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
