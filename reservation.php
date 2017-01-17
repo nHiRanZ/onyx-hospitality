99ea37c6b06cc07d4f1ba690b472f8b652d85ea7
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-17 12:16:03 +0530

    titles changed

--- reservation.php
+++ reservation.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>Game Introduction | ONYX Hospitality</title>
+    <title>Reservations | ONYX Hospitality</title>
 
     <?php include('head.php'); ?>
     <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
