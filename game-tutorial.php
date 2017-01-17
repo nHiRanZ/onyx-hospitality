6838d3aed369b6b3bfa1f80b039c7ab25eb7bdc6
Lishni Tilakaratne <lishni.tilakaratne@gmail.com>
2017-01-17 17:44:42 +0530

    game tutorial page crrected

--- game-tutorial.php
+++ game-tutorial.php
@@ -9,23 +9,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>Game Introduction | ONYX Hospitality</title>
+    <title>Game Tutorial | ONYX Hospitality</title>
 
     <?php include('head.php'); ?>
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "350px";
+        }
 
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    </script>
 </head>
 
 <body style="height: 100%">
 
 <div class="container col-md-12 col-sm-12 col-xs-12" style="">
     <div class="highlight">
-        <span><a href="index.php"><i class="fa fa-angle-left fa-3x" style="margin: 15px 15px; "></i></a></span>
+        <span style="font-size:30px;cursor:pointer;margin: 15px 22px;" onclick="openNav()">&#9776;</span>
         <a href="index.php"><img src="img/logo/onyx-hospitality-logo-white.png" alt="Logo" class="highlightlogo"></a>
     </div>
 
     <div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
 
         <img src="img/game/game-intro.jpg" style="">
+        <?php
+        include('side-nav.php');
+        ?>
     </div>
 
     <div class="col-md-6 col-sm-6 col-xs-12 containertext center" style="">
